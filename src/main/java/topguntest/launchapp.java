3fbfbf2d013a575960f05fa4e685d7c8c0a9438c
Maciek Konkolowicz <mkonkolowicz@gmail.com>
2016-10-31 16:32:33 -0400

    Initial commit. The appium test relies on an appium server being launched and then launches the emulator

---
+++ src/main/java/topguntest/launchapp.java
@@ -0,0 +1,27 @@
+package topguntest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.ios.IOSDriver;
+import java.net.URL;
+
+public class launchapp 
+{
+	public static void main(String[] args) throws Exception {
+		 
+	    DesiredCapabilities capabilities = new DesiredCapabilities();
+	    capabilities.setCapability("platformName", "iOS");
+	    capabilities.setCapability("deviceName", "iPhone 6s Plus");
+	    capabilities.setCapability("platformVersion", "10.0");
+	    capabilities.setCapability("app", "/Users/mkonkolowicz-/Documents/Apps/TopGunSyllabusiOS.ipa");
+	    capabilities.setCapability("browserName", "");
+	    capabilities.setCapability("deviceOrientation", "portrait");
+	    capabilities.setCapability("appiumVersion", "1.6.0");
+	 
+	    WebDriver driver = new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+	    /**
+	     * 0.0.0.0:4723
+	     * Test Actions here...
+	     */
+	    driver.quit();
+	}	
+}
