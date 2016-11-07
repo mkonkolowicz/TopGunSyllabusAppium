110610877fca48673d613a8670aa247daf3f7896
Maciek Konkolowicz <mkonkolowicz@gmail.com>
2016-11-07 15:06:56 -0500

    updated capabilities to proper device name and new app built for the iOS emulator

--- src/main/java/topguntest/launchapp.java
+++ src/main/java/topguntest/launchapp.java
@@ -10,18 +10,14 @@ public class launchapp
 		 
 	    DesiredCapabilities capabilities = new DesiredCapabilities();
 	    capabilities.setCapability("platformName", "iOS");
-	    capabilities.setCapability("deviceName", "iPhone 6s Plus");
+	    capabilities.setCapability("deviceName", "iPhone 6s");
 	    capabilities.setCapability("platformVersion", "10.0");
-	    capabilities.setCapability("app", "/Users/mkonkolowicz-/Documents/Apps/TopGunSyllabusiOS.ipa");
-	    capabilities.setCapability("browserName", "");
+	    capabilities.setCapability("app", "/Users/mkonkolowicz-/Documents/Apps/TopGunSyllabusiPhone.app");
 	    capabilities.setCapability("deviceOrientation", "portrait");
-	    capabilities.setCapability("appiumVersion", "1.6.0");
+	    //capabilities.setCapability("appiumVersion", "1.6.0");
 	 
 	    WebDriver driver = new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
-	    /**
-	     * 0.0.0.0:4723
-	     * Test Actions here...
-	     */
+	    
 	    driver.quit();
 	}	
 }
